b1e5e60039dfb725e174976d280283cc28dca036
jachin <2061979589@qq.com>
2021-07-16 09:15:56 +0800

    m

---
+++ templates/第3章：使用Vue脚手架/vue_staging/1_src_hello/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="welcome">欢迎学习Vue</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+    components: {HelloWorld}
+}
+</script>
+
+<style>
+.welcome{
+    background-color: gray;
+}
+</style>
